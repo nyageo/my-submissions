0a410d5cb7803f3db82b8d84f9ccddede0debb23
nyageo <139861941+nyageo@users.noreply.github.com>
2024-07-15 21:40:00 +0300

    Upd Exercise 1.1.-1.2

--- part1/Exercises 1.1.-1.2/src/App.jsx
+++ part1/Exercises 1.1.-1.2/src/App.jsx
@@ -1,6 +1,44 @@
 import { useState } from 'react'
+
+const Header = (p) => {
+  console.log(p)
+  return (
+    <div>
+      <h1> {p.course}</h1>
+    </div>
+  )
+}
+
+const Part =(pr) =>{
+  return(
+    <p>
+      {pr.part}{pr.exercise}
+    </p>
+  )
+}
+const Content =(pr) =>{
+  return(
+    <p>
+      <Part part= 'Fundamentals of React: ' exercise= '10'/>
+      <Part part= 'Using props to pass data: ' exercise= '7'/>
+      <Part part= 'State of a component: ' exercise= '14'/>
+    </p>
+  )
+}
+
+const Total =(pr) =>{
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return(
+    <p>
+     Number of exercises {exercises1 + exercises2 + exercises3}
+      
+    </p>
+  )
+}
 const App = () => {
-  const course = 'Half Stack application development'
+  
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -10,18 +48,10 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+      <Header course='Half Stack application development' />
+      <Content />
+      <Total />  
+     </div>
   )
 }
 
